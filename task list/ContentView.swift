5dc81ca2e27782be6acb16a5c2d57ad70b738724
Azhagusundaram.t <Azhagusundaram.t@zohocorp.com>
2020-02-20 13:28:56 +0530

    Initial Commit

---
+++ task list/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  task list
+//
+//  Created by Azhagusundaram Tamil on 20/02/20.
+//  Copyright © 2020 Azhagusundaram Tamil. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
